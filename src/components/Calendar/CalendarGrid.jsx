c0fed5980259d51fe26dabad93d830b970609028
Max Rudin <rudin.maksimka@bk.ru>
2022-11-23 16:23:06 +0500

    CurraentDAY

--- src/components/Calendar/CalendarGrid.jsx
+++ src/components/Calendar/CalendarGrid.jsx
@@ -1,3 +1,4 @@
+import moment from 'moment/moment';
 import React from 'react'
 import styled from 'styled-components';
 import './Calendar.css'
@@ -9,24 +10,25 @@ const RowInCell = styled.div`
 const CallWrapper = styled.div`
     background-color:${props => props.isWeekend ? '#272829' : '#1e1f21'}
 `
-function CalendarGrid({startDay}) {
-    // const totalday = 42;
-    const day = startDay.clone().subtract(1, 'day');
-    const daysArray = [...Array(42)].map(() => day.add(1, 'day').clone())
-    // console.log(daysArray)
 
+function CalendarGrid({startDay}) {
+    const day = startDay.clone().subtract(1, 'day');
+    const daysArray = [...Array(42)].map(() => day.add(1, 'day').clone());
+
+    const isCurraentDAY = (day) => moment().isSame(day, 'day');
     return (
         <div className='GridWrapper'>
             {
                 daysArray.map((dayItem) => (
                     <CallWrapper 
-                    className='CallWrapper'
-                    key={dayItem.format('DD-MM-YYYY')}
-                    isWeekend={dayItem.day() === 6 || dayItem.day() === 0}
+                        className='CallWrapper'
+                        key={dayItem.format('DD-MM-YYYY')}
+                        isWeekend={dayItem.day() === 6 || dayItem.day() === 0}
                     >
                         <RowInCell justifyContent={'flex-end'}>
                             <div className='DayWrapper'>
-                                {dayItem.format('D')}
+                                {isCurraentDAY(dayItem) ? <div className='CurraentDAY'>{dayItem.format('D')}</div>
+                                : dayItem.format('D')}
                             </div>
                         </RowInCell>
                     </CallWrapper>
